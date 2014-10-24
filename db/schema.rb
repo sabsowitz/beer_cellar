4c4f93cbb0edc2f2acf1ce1765bdd7a13d3c0756
sabsowitz <sabsowitz@yahoo.com>
2014-10-24 17:44:42 -0400

    Added tests for name and email validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024202051) do
+ActiveRecord::Schema.define(version: 20141024213602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,5 +32,7 @@ ActiveRecord::Schema.define(version: 20141024202051) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
 
   it { should be_valid }
 
+  # Name Validations
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
@@ -27,4 +28,60 @@ describe User do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
+
+  # Email Validations
+
+  describe "email" do
+    context "not present" do
+    before { @user.email = " " }
+      it { should_not be_valid }
+    end
+  end
+
+  context "too long" do
+    before { @user.email = "a" * 201 }
+    it { should_not be_valid }
+  end
+
+  describe "correct format" do
+    it "is valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  describe "incorrect format " do
+    it "is invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                  foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).to_not be_valid
+      end
+    end
+  end
+
+  describe "mixed case" do
+    it "is saved as lowercase" do
+      mixed_case_email = "fooBAR@exaMPlE.coM"
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
+  describe "when already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+  end
+
+  it { should_not be_valid }
+
+  User.destroy_all
 end 
