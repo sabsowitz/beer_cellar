556fc8261307f41c415250ecd064f9378cb44933
sabsowitz <sabsowitz@yahoo.com>
2014-10-29 09:33:54 -0400

    Working on API

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -2,6 +2,7 @@ class BeersController < ApplicationController
   def index
     # # @response = HTTParty.get "http://api.brewerydb.com/v2/search?q=firestone&type=brewery&key=4938dd57b16650c7920500ed375f44e5"
     @beers = Beer.all
+    @response = Beer.party(params[:beer_name])
     # @beer = Beer.party(params[:beer_name])
   end
 
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,8 +3,10 @@ class Beer < ActiveRecord::Base
   def self.party(beer_name)
     beer_name ||= "stone"
 
+    beer_for_api_call = beer_name.gsub(" ", "+")
+
     # auth = { query: { key: '4938dd57b16650c7920500ed375f44e5'} }
-    beer_url = "http://api.brewerydb.com/v2/search?q=#{beer_name}&type=beer&results=10&key=4938dd57b16650c7920500ed375f44e5"
+    beer_url = "http://api.brewerydb.com/v2/search?q=#{beer_for_api_call}&type=beer&results=10&key=4938dd57b16650c7920500ed375f44e5"
     response = HTTParty.get beer_url
   end
 end
