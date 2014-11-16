38629a4a53e50b8be523edeb1e4aa2315e8df6fc
sabsowitz <sabsowitz@yahoo.com>
2014-11-16 14:13:32 -0500

    Started Angular JS and Bower to project

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+
+gem 'angularjs-rails'
 gem 'rails_12factor', group: :production
 gem 'httparty'
 gem 'twitter-bootstrap-rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
 
+//= require angular
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require turbolinks
 //= require bootstrap
 //= require bootstrap-sprockets
 //= require_tree .
