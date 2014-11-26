df24a0aefb145a10dc8a22351c30bb8874a19260
sabsowitz <sabsowitz@yahoo.com>
2014-11-26 13:17:53 -0500

    Updates static page

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
   match '/about',     to: 'static_pages#about',     via: 'get'
   match '/help',      to: 'static_pages#help',      via: 'get'
   match '/contact',   to: 'static_pages#contact',   via: 'get'
+  match '/updates',   to: 'static_pages#updates',   via: 'get'
 end
